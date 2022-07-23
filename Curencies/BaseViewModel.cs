b11c25d9086e819b90b3d2ff5e73743622aa9f3d
Lucien Leroy Siani <LucienLeroy.Siani@dbh.de>
2022-07-23 17:23:38 +0200

    adding existing project files to git

---
+++ Curencies/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curencies
+{
+	internal class BaseViewModel : INotifyPropertyChanged
+	{
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		public virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			if (!string.IsNullOrEmpty(propertyName))
+				this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ Curencies/CurencyViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curencies
+{
+	internal class CurencyViewModel : BaseViewModel
+	{
+
+		private decimal _value;
+		public decimal Value
+		{
+			get => _value; set
+			{
+				if (_value != value)
+					_value = value;
+			}
+		}
+	}
+}
