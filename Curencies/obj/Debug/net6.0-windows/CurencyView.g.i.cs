97e6c456bc10a502b9e768ec587ebf9f508290c2
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-07-24 12:49:30 +0200

    updating workspace

--- Curencies/obj/Debug/net6.0-windows/CurencyView.g.i.cs
+++ Curencies/obj/Debug/net6.0-windows/CurencyView.g.i.cs
@@ -48,13 +48,13 @@ namespace Curencies {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Curencies;V1.0.0.0;component/curencyview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Curencies;component/curencyview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\CurencyView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -64,7 +64,7 @@ namespace Curencies {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.6.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
