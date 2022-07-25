1a96614ad8bdbe0b4bc16a927af0030e6d5d985c
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-07-25 23:54:22 +0200

    adding dynamic library class

---
+++ PersonList/Person.cs
@@ -0,0 +1,53 @@
+﻿using MyDll;
+
+namespace PersonList
+{
+    public class Person : NotifyableObject
+    {
+        private string firstname;
+        private string lastname;
+        private string departement;
+        public string FullName => $"{firstname} {lastname}";
+
+        public string Firstname
+        {
+            get => firstname; set
+            {
+                if (value != firstname)
+                {
+                    firstname = value;
+                    this.RaisePropertyChanged();
+                    this.RaisePropertyChanged(nameof(FullName));
+                }
+            }
+        }
+
+        public string Lastname
+        {
+            get => lastname; set
+            {
+                if (value != lastname)
+                {
+                    lastname = value;
+                    this.RaisePropertyChanged();
+                    this.RaisePropertyChanged(nameof(FullName));
+                }
+            }
+        }
+
+        public string Departement
+        {
+            get => departement; set
+            {
+                if (value != departement)
+                {
+                    departement = value;
+                    this.RaisePropertyChanged();
+
+                }
+            }
+        }
+
+
+    }
+}
---
+++ WpfBasics/NotifyableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyDll
+{
+    public class NotifyableObject : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (!string.IsNullOrEmpty(propertyName))
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
