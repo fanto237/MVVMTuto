8632da3aebd4c17c84f8d6da64c2a2de91d6ec39
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-07-25 23:54:58 +0200

    updating currencies project

--- Curencies/obj/Debug/net6.0-windows/CurencyView.g.i.cs
+++ Curencies/obj/Debug/net6.0-windows/CurencyView.g.i.cs
@@ -54,7 +54,7 @@ namespace Curencies {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Curencies;component/curencyview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Curencies;V1.0.0.0;component/curencyview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\CurencyView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
